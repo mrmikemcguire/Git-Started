11ad2bf889093a29aa67b429acfda164f99623b3
Mike McGuire <MoonshotMike@gmail.com>
2015-12-01 15:23:25 -0700

    tabs not working

--- gitStarted.js
+++ gitStarted.js
@@ -1,1 +1,5 @@
-//This is my external JS file
+$('tab1').load('first.html');
+$('tab2').load('untracked.html');
+$('tab3').load('staged.html');
+$('tab4').load('committed.html');
+$('tab5').load('pushed.html');
