5ea0ff52d941b9dd83ccf314825b1bb3cb3af6e4
Mike McGuire <mcguire@mullenhigh.com>
2015-12-03 10:10:11 -0700

    move array to js file

--- gitStarted.js
+++ gitStarted.js
@@ -5,3 +5,45 @@ $(document).on('click','.navbar-collapse.in',function(e)
         $(this).collapse('hide');
         }
     });
+
+(function()
+    {
+    var app = angular.module('commands', []);
+    
+    app.controller('CommandsController', function()
+        {
+        this.instruction = commands;
+        });
+    
+    var commands =
+        {
+        label: "push",
+        command: "git push",
+        AKA: "upload",
+        variations: 
+            [
+            "git push -u origin master"  
+            ],
+        level: "committed",
+        images:
+            [
+            "images/correctPush.png",
+            "images/wrongPush.png"
+            ]
+        },
+        {
+        label: "add",
+        command: "git add .",
+        AKA: "track",
+        variations: 
+            [
+            "git add nameOfFile.txt"
+            ],
+        level: "untracked",
+        images:
+            [
+            "images/correctAdd.png",
+            "images/wrongAdd.png"
+            ]
+        }
+    })();
