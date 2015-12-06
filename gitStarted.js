5957723218a1fe20910bc20594803f527dafa706
Mike McGuire <MoonshotMike@gmail.com>
2015-12-06 06:53:55 -0700

    bracket fix attempt

--- gitStarted.js
+++ gitStarted.js
@@ -7,59 +7,27 @@ $(document).on('click','.navbar-collapse.in',function(e)
         }
     });
 
+
 (function()
     {
     var gitStarted = angular.module('gitStarted', []);
-    
-    gitStarted.controller('CLIController', function($scope)
-        {
-        $scope.instructions = commands;
-        });
-    
+
     gitStarted.controller('arrayController', ['$http', function($http)
         {
         var array = this;
         array.commands = [];
-        $http.get(commands.JSON).success(function(data)
+        $http.get('commands.JSON').success(function(data)
               {
-              some.commands = data;
-              }
-        })];
-            
+              array.commands = data;
+              });
+        }]);
+    
+    gitStarted.controller('CLIController', function($scope)
+        {
+        $scope.instructions = commands;
+        });          
      })();
     
-//    var commands =
-//        [{
-//        label: "push",
-//        command: "git push",
-//        AKA: "upload",
-//        variations: 
-//            [
-//            "git push -u origin master"  
-//            ],
-//        level: "committed",
-//        images:
-//            [
-//            "images/correctPush.png",
-//            "images/wrongPush.png"
-//            ]
-//        },
-//        {
-//        label: "add",
-//        command: "git add .",
-//        AKA: "track",
-//        variations: 
-//            [
-//            "git add nameOfFile.txt"
-//            ],
-//        level: "untracked",
-//        images:
-//            [
-//            "images/correctAdd.png",
-//            "images/wrongAdd.png"
-//            ]
-//        }
-//    ];
     
 
 
